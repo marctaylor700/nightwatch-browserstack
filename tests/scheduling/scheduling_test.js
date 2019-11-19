211c9e165057c9f1258dd391ced6d5554deb1f33
Marc Taylor <marctaylor@Marcs-MacBook-Pro.local>
2019-11-19 15:12:59 -0700

    updated scheduled visit test for provider

--- tests/scheduling/scheduling_test.js
+++ tests/scheduling/scheduling_test.js
@@ -15,40 +15,40 @@ async function goToPracticeLoginPage(browser, handle) {
 
 async function providerLogin(browser) {
   console.log("starting provider login")
-  //use css selectors
-  browser.useXpath()
   //email field
-  browser.expect.element('/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div/div[1]/div/div/div[2]/div/div/div/div[2]/form/div[1]/div/div[1]/input').to.be.present;
-  browser.click('/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div/div[1]/div/div/div[2]/div/div/div/div[2]/form/div[1]/div/div[1]/input')
+  browser.useXpath()
+  browser.expect.element('/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div/div[1]/div/div/div[2]/div/div/div/div[2]/form/div[1]/div/div[1]/input').to.be.present;
+  browser.click('/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div/div[1]/div/div/div[2]/div/div/div/div[2]/form/div[1]/div/div[1]/input')
   //set email
-  browser.setValue('/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div/div[1]/div/div/div[2]/div/div/div/div[2]/form/div[1]/div/div[1]/input', "physician+nick@evisit.com")
+  browser.setValue('/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div/div[1]/div/div/div[2]/div/div/div/div[2]/form/div[1]/div/div[1]/input', "physician+nick@evisit.com")
   //password field
-  browser.expect.element('/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div/div[1]/div/div/div[2]/div/div/div/div[2]/form/div[2]/div/div[1]/input').to.be.present;
-  browser.click("/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div/div[1]/div/div/div[2]/div/div/div/div[2]/form/div[2]/div/div[1]/input")
+  browser.expect.element('/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div/div[1]/div/div/div[2]/div/div/div/div[2]/form/div[2]/div/div[1]/input').to.be.present;
+  browser.click("/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div/div[1]/div/div/div[2]/div/div/div/div[2]/form/div[2]/div/div[1]/input")
   //set password
-  browser.setValue("/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div/div[1]/div/div/div[2]/div/div/div/div[2]/form/div[2]/div/div[1]/input", "provider123")
+  browser.setValue("/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div/div[1]/div/div/div[2]/div/div/div/div[2]/form/div[2]/div/div[1]/input", "provider123")
   //login button
-  browser.expect.element('/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div/div[1]/div/div/div[2]/div/div/div/div[2]/div[2]/div[2]/div/div/span').to.be.present;
+  browser.expect.element('/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div/div[1]/div/div/div[2]/div/div/div/div[2]/div[2]/div[2]/div/div').to.be.present;
   //click login
-  browser.click("/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div/div[1]/div/div/div[2]/div/div/div/div[2]/div[2]/div[2]/div/div/span")
+  browser.click("/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div/div[1]/div/div/div[2]/div/div/div/div[2]/div[2]/div[2]/div/div")
   //wait for page to load
-  browser.pause(5000)
+  browser.pause(7000)
 }
 
 
 async function schedulingTab(browser, newUserCredentials) {
   //console log test 
+  browser.useXpath()
   console.log("starting scheduling test")
   //expect scheduling tab
-  browser.expect.element("/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[1]/div[4]/div/div/span").to.be.present;
+  browser.expect.element("/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[1]/div[4]/div").to.be.present;
   //click scheduling tab
-  browser.click("/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[1]/div[4]/div/div/span")
+  browser.click("/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[1]/div[4]/div")
   //pause
   browser.pause(5000)
   //expect visit type dropdown
-  browser.expect.element("/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[1]/div/div/div/div/div[1]/input").to.be.present;
+  browser.expect.element("/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[1]/div/div/div/div/div[1]").to.be.present;
   //click visit type dropdown
-  browser.click("/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[1]/div/div/div/div/div[1]/input")
+  browser.click("/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[1]/div/div/div/div/div[1]")
   //pause
   browser.pause(2000)
   //expect scheduled visit 
@@ -56,9 +56,9 @@ async function schedulingTab(browser, newUserCredentials) {
   //click scheduled visit
   browser.click("/html/body/div/div/div/div[2]/div/div/div/div/div/div/div[2]")
   //expect patient dropdown
-  browser.expect.element("/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[2]/div/div/div/div[1]/input").to.be.present;
+  browser.expect.element("/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[2]/div/div/div/div[1]").to.be.present;
   //click patient dropdown
-  browser.click("/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[2]/div/div/div/div[1]/input")
+  browser.click("/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[2]/div/div/div/div[1]")
   //expect create new patient
   browser.expect.element("/html/body/div/div/div/div[2]/div/div/div/div[1]/div[2]/div/div/span").to.be.present;
   //click new patient
@@ -86,49 +86,58 @@ async function schedulingTab(browser, newUserCredentials) {
   //set email
   browser.setValue("/html/body/div/div/div/div[2]/div/div/div/div[1]/div[1]/div/form/div[4]/div/div[1]/input", newUserCredentials.email)
   //expect create patient 
-  browser.expect.element("/html/body/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[1]/div/span").to.be.present;
+  browser.expect.element("/html/body/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[1]/div").to.be.present;
   //click create patient
-  browser.click("/html/body/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[1]/div/span")
+  browser.click("/html/body/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[1]/div")
   //pause
-  browser.pause(3000)
+  browser.pause(5000)
   //expect provider field
-  browser.expect.element("/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[3]/div/div/div/div[1]/input").to.be.present;
+  browser.expect.element("/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[3]/div/div/div/div[1]").to.be.present;
   //click provider field
-  browser.click("/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[3]/div/div/div/div[1]/input")
+  browser.click("/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[3]/div/div/div/div[1]")
   //input nick
-  browser.setValue("/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[3]/div/div/div/div[1]/input", "Nick")
+  //browser.setValue("/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[3]/div/div/div/div[1]", "Nick")
   //pause
   browser.pause(3000)
   //expect nick's provider
-  browser.expect.element("/html/body/div/div/div/div[2]/div/div/div/div[1]/div/div/div/div/div[2]/span[1]").to.be.present;
+  browser.expect.element("/html/body/div/div/div/div[2]/div/div/div/div[1]/div/div/div[3]/div/div[2]/span[2]").to.be.present;
   //click nick
-  browser.click("/html/body/div/div/div/div[2]/div/div/div/div[1]/div/div/div/div/div[2]/span[1]")
+  browser.click("/html/body/div/div/div/div[2]/div/div/div/div[1]/div/div/div[3]/div/div[2]/span[2]")
   //expect date field
-  browser.expect.element("/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[4]/div/div/div/div/div[1]/input").to.be.present;
+  browser.expect.element("/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[4]/div/div/div/div/div[1]/input").to.be.present;
   //click date field
-  browser.click("/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[4]/div/div/div/div/div[1]/input")
+  browser.click("/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[4]/div/div/div/div/div[1]/input")
+  // had to remove the commented piece as browserstack never finds the arrow button and in turn just times out the test each time. 
   //expect arrow to go to next month
-  browser.expect.element('/html/body/div/div/div/div[2]/div/div/div/div[1]/div/div/div/div/div[1]/div/div[2]/div/div/div/span')
-  //click arrow to go to next month
-  browser.click('/html/body/div/div/div/div[2]/div/div/div/div[1]/div/div/div/div/div[1]/div/div[2]/div/div/div/span')
-  //expect one date. need to figure out how to make this dynamic so the date can be called to any date we would want. 
-  browser.expect.element("/html/body/div/div/div/div[2]/div/div/div/div[1]/div/div/div/div/div[3]/div[2]/div[6]/span/span").to.be.present;
+  // browser.expect.element('/html/body/div/div/div/div[2]/div/div/div/div[1]/div/div/div/div/div[1]/div/div[2]/div/div/div/span')
+  // //click arrow to go to next month
+  // browser.click('/html/body/div/div/div/div[2]/div/div/div/div[1]/div/div/div/div/div[1]/div/div[2]/div/div/div/span')
+  // //expect one date. need to figure out how to make this dynamic so the date can be called to any date we would want. 
+  browser.expect.element("/html/body/div/div/div/div[2]/div/div/div/div[1]/div/div/div/div/div[3]/div[4]/div[6]/span/span").to.be.present;
   //click date
-  browser.click("/html/body/div/div/div/div[2]/div/div/div/div[1]/div/div/div/div/div[3]/div[2]/div[6]/span/span")
-  // //expect time field
-  // browser.expect.element("/html/body/div/div/div/div[2]/div/div/div/div[1]/div/div/div/div[1]/div/span").to.be.present;
-  // //click 8am time
-  // browser.click("/html/body/div/div/div/div[2]/div/div/div/div[1]/div/div/div/div[1]/div/span")
-  // //expect schedule visit button
-  // browser.expect.element("/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[6]/div/div").to.be.present;
-  // //click schedule visit 
-  // browser.click("/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[6]/div/div")
-  // //pause
-  // browser.pause(6000)
-  // //expect decline button to cancel the visit 
-  // browser.expect.element("/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[2]/div[2]/div/div[4]/div/div[4]/div[1]/div/div/div/span").to.be.present;
-  // //click cancel button
-  // browser.click("/html/body/div/div/div/div[1]/div[1]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[2]/div[2]/div/div[4]/div/div[4]/div[1]/div/div/div/span")
+  browser.click("/html/body/div/div/div/div[2]/div/div/div/div[1]/div/div/div/div/div[3]/div[4]/div[6]/span/span")
+  //expect time field
+  browser.expect.element("/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[5]/div/div/div/div/div[1]/input").to.be.present;
+  //click time field
+  browser.click("/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[5]/div/div/div/div/div[1]/input")
+  //expect 8am time
+  browser.expect.element("/html/body/div/div/div/div[2]/div/div/div/div[1]/div/div/div/div[5]/div").to.be.present;
+  //click 8am time
+  browser.click("/html/body/div/div/div/div[2]/div/div/div/div[1]/div/div/div/div[5]/div")
+  //expect schedule visit button
+  browser.expect.element("/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[6]/div/div").to.be.present;
+  //click schedule visit 
+  browser.click("/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/form/div[6]/div/div")
+  //pause
+  browser.pause(6000)
+  //expect decline button to cancel the visit 
+  browser.expect.element("/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[2]/div[2]/div/div[6]/div/div[4]/div[1]/div/div/div/span").to.be.present;
+  //click cancel button
+  browser.click("/html/body/div/div/div/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[2]/div[2]/div/div[6]/div/div[4]/div[1]/div/div/div/span")
+  //expect yes no option
+  browser.expect.element("/html/body/div/div/div/div[2]/div/div/div/div/div/div/div/div/div[3]/div[3]/div").to.be.present;
+  //click yes
+  browser.click("/html/body/div/div/div/div[2]/div/div/div/div/div/div/div/div/div[3]/div[3]/div")
   //pause
   browser.pause(9000)
 
