3826dcf0cb38ed29edc850dc058e6578c7cb7dd6
Marc Taylor <marctaylor@Marcs-MacBook-Pro.local>
2019-12-20 10:11:44 -0700

    added data test ids

--- tests/provider/prescription_test.js
+++ tests/provider/prescription_test.js
@@ -10,42 +10,39 @@ async function goToPracticeLoginPage(browser, handle) {
 async function providerLogin(browser) {
     console.log("starting provider login")
     browser.useCss()
-    //email field
-    browser.expect.element(`[data-field='email']`).to.be.present;
-    //browser.click(`div[data-field='email']`)
+    //check for email field
+    browser.expect.element(`[data-test-id='emailTestID']`).to.be.present;
     //set email
-    browser.setValue(`[data-field='email'] input`, "physician+nick@evisit.com")
-    //password field
-    browser.expect.element(`[data-field='password']`).to.be.present;
-    //browser.click(`input[name='current-password']`)
+    browser.setValue(`[data-test-id='emailTestID']`, "physician+nick@evisit.com")
+    //check for password field
+    browser.expect.element(`[data-test-id='passwordTestID']`).to.be.present;
     //set password
-    browser.setValue(`[data-field='password'] input`, "provider123")
+    browser.setValue(`[data-test-id='passwordTestID']`, "provider123")
     // //login button
-    // browser.expect.element(`div[class='raTouchable raView eVisitAppButton eVisitAppButtonContainer eVisitAppButtonWithCaption eVisitAppComponent_0000000000321']`).to.be.present;
+    browser.expect.element(`[data-test-id='loginButtonTestID']`).to.be.present;
     // //click login
-    // browser.click(`div[class='raTouchable raView eVisitAppButton eVisitAppButtonContainer eVisitAppButtonWithCaption eVisitAppComponent_0000000000321']`)
+    browser.click(`[data-test-id='loginButtonTestID']`)
     // //wait for page to load
-    // browser.pause(7000)
+    browser.pause(7000)
   }
 
   async function visitHistoryTab(browser) {
     console.log("starting visit history tab test")
-  //switch back to xpath. 
   browser.useCss()
   //visit history tab
-  browser.expect.element('div.raTouchable:nth-child(3)').to.be.present;
+  browser.expect.element(`[data-test-id='visitHistoryTabTestID']`).to.be.present;
   //click tab
-  browser.click("div.raTouchable:nth-child(3)")
+  browser.click(`[data-test-id='visitHistoryTabTestID']`)
   //wait for visit history tab to load
   browser.pause(5000)
   //click on visit state
-  browser.expect.element(`input[name='completedStatus']`).to.be.present;
-  browser.click(`input[name='completedStatus']`)
-  browser.pause(2000)
-  browser.expect.element(".eVisitAppComponent_0000000000362").to.be.present;
-  //browser.expect.element(`span[class='RAText']`).text.to.equal('All');
-  browser.click(`div[class='raView eVisitAppPopupMenuItem eVisitAppCo`)
-  browser.pause(5000)
+  // browser.expect.element(`input[name='completedStatus']`).to.be.present;
+  // browser.click(`input[name='completedStatus']`)
+  // browser.pause(2000)
+  // browser.expect.element(".eVisitAppComponent_0000000000362").to.be.present;
+  // //browser.expect.element(`span[class='RAText']`).text.to.equal('All');
+  // browser.click(`div[class='raView eVisitAppPopupMenuItem eVisitAppCo`)
+  // browser.pause(5000)
   }
 
 
@@ -61,7 +58,7 @@ module.exports = {
     'Provider Login - Verify Visit History Tab' : async function(browser) {
       goToPracticeLoginPage(browser, "ewellness")
         .then(providerLogin(browser))
-        // .then(visitHistoryTab(browser))
+        .then(visitHistoryTab(browser))
         // .then(browser.end());
     }, 
   };
