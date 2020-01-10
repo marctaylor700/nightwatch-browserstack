a7263e174579046a45645e83f7bafcf947c13ea3
Marc Taylor <marctaylor@Marcs-MacBook-Pro.local>
2020-01-10 10:54:15 -0700

    add project name to patient config

--- conf/patient.conf.js
+++ conf/patient.conf.js
@@ -15,6 +15,7 @@ nightwatch_config = {
           'os': 'OS X',
           'os_version': 'Mojave',
           'build' : 'Patient Tests',
+          'project' : 'eVisit Nightwatch',
           'browser': 'Chrome',
           'browser_version': '78.0 beta',
           'resolution': '1920x1080'
