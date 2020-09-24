6a60d4511f498f5a721eb15c7868db9a66664a39
Felipe Augusto Schiquetti <felipe.schiquetti@arctouch.com>
2020-09-24 15:49:42 -0300

    changed the way the clearValue function works
    This will improve the performance on browserstack, since now it will send only one command to erase all chars instead of several commands erasing one char at the time.

--- commands/clearValue2.js
+++ commands/clearValue2.js
@@ -1,10 +1,13 @@
 exports.command = function clearValue2(selector) {
-  const { RIGHT_ARROW, BACK_SPACE } = this.Keys
-  return this.getAttribute(selector, 'value', (result) => {
-    const chars = result.value.split('')
-    // Make sure we are at the end of the input
-    chars.forEach(() => this.setValue(selector, RIGHT_ARROW))
-    // Delete all the existing characters
-    chars.forEach(() => this.setValue(selector, BACK_SPACE))
-  })
+  let self = this;
+  let backspace = []
+
+  this.getAttribute('css selector', selector, 'value', function (result) {
+      for (i = 0; i < result.value.length; i++) {
+          backspace.push('\u0008');
+      }
+      self.setValue(selector, backspace)
+  });
+
+  return this
 }
