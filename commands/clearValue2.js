5593f6d276a8e4b4914b28dde62781fba45edc8a
Felipe Augusto Schiquetti <51936874+arctouch-felipeschiquetti@users.noreply.github.com>
2020-09-24 16:23:30 -0300

    Update clearValue2.js

--- commands/clearValue2.js
+++ commands/clearValue2.js
@@ -1,12 +1,9 @@
 exports.command = function clearValue2(selector) {
   let self = this;
-  let backspace = []
 
   this.getAttribute('css selector', selector, 'value', function (result) {
-      for (i = 0; i < result.value.length; i++) {
-          backspace.push('\u0008');
-      }
-      self.setValue(selector, backspace)
+    var backspace = new Array(result.value.length + 1).join('\u0008');
+    self.setValue(selector, backspace)
   });
 
   return this
