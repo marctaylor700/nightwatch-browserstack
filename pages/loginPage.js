da394f3ee1992ec102f723a76018d0110856441c
Felipe Augusto Schiquetti <felipe.schiquetti@arctouch.com>
2020-09-29 15:02:26 -0300

    reset password test for valid and invalid email

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -1,7 +1,22 @@
 const elements = {
     email: `[data-test-id='email']`,
     password: `[data-test-id='password']`,
-    btnLogin: `[data-test-id='loginButton']`
+    btnLogin: `[data-test-id='loginButton']`,
+    btnForgotPassword: `[data-test-id="forgotPassword"]`,
+    btnRegistration: `[data-test-id="dontHaveAccount"]`,
+
+    textForgotPassword: `.eVisitAppLoginPageMessageContainer`,
+    textEmailNotFound: `.eVisitAppLoginPageErrorMessageContainer`,
+    emailForgotPassword: `[name="email"]`,
+    btnSendForgotPassword: `[data-test-id="resetPassword"]`,
+    btnCancelForgotPassword: `[data-test-id="cancelReset"]`,
+    btnDoneForgotPassword: `[data-test-id="cancelReset"]`,
+
+    confirmPassword: `[data-test-id="confirmPassword"]`,
+    termsCheckbox: `[data-test-id="tos0"]`,
+    btnRegister: `[data-test-id="register"]`,
+    btnAlreadyRegistered: `[data-test-id="alreadyRegistered"]`
+
 };
 
 const commands = [{
@@ -26,7 +41,34 @@ const commands = [{
             .click('@btnLogin')
 
         return this
-    }
+    },
+
+    forgotPasswordKnownEmail(user_email) {
+        this
+            .waitForElementVisible('@email')
+            .click('@btnForgotPassword')
+            .waitForElementVisible('@emailForgotPassword')
+            .setValue('@emailForgotPassword', user_email)
+            .click('@btnSendForgotPassword')
+            .expect.element(`@textForgotPassword`).text.to.contain("Your Email Has Been Sent.\nPlease follow the instructions provided to reset your password. Don't forget to check your spam folder.")
+            this.click('@btnDoneForgotPassword')
+        return this
+    },
+
+
+    forgotPasswordUnknownEmail() {
+        this
+            .waitForElementVisible('@email')
+            .click('@btnForgotPassword')
+            .waitForElementVisible('@emailForgotPassword')
+            .setValue('@emailForgotPassword', "wrong_password@evisit.com")
+            .click('@btnSendForgotPassword')
+            .expect.element(`@textEmailNotFound`).text.to.contain("Email not found")
+            this.click('@btnCancelForgotPassword')
+        return this
+    },
+
+
 
 }];
 
