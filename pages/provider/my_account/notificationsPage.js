fc5304be7ca847038cc4ecd5251ec47cecb6f2d4
Jeanne Trovão <jeanne.trovao@arctouch.com>
2020-10-23 18:41:35 -0300

    add test to change Notify checkboxes and check persistence

--- pages/provider/my_account/notificationsPage.js
+++ pages/provider/my_account/notificationsPage.js
@@ -21,15 +21,39 @@ const elements = {
     customPhoneNotifField:`[name^="custom.voice"]`,    
     customEmailNotifField:`[name^="custom.email"]`,
    
-    //Toggles of fixed notifications channels and the first custom channels
+    //Toggles of fixed notification channels
     smsNotifToggle: {selector: `[data-test-id="smsToggle"] [class="RAView eVisitAppSwitchFieldKnob"]`, index: 0},
     phoneNotifToggle: {selector: `[data-test-id="voiceToggle"] [class="RAView eVisitAppSwitchFieldKnob"]`, index: 0},
     emailNotifToggle: {selector: `[data-test-id="emailToggle"] [class="RAView eVisitAppSwitchFieldKnob"]`, index: 0},
+
+    //Toggles of first custom notification channels
     customSmsNotifToggle: {selector: `[data-test-id="smsToggle"] [class="RAView eVisitAppSwitchFieldKnob"]`, index: 1},
     customPhoneNotifToggle: {selector: `[data-test-id="voiceToggle"] [class="RAView eVisitAppSwitchFieldKnob"]`, index: 1},
     customEmailNotifToggle: {selector: `[data-test-id="emailToggle"] [class="RAView eVisitAppSwitchFieldKnob"]`, index: 1},
 
     removeChannelButton: `[data-test-id="notificationModalRemove"]`,
+
+    //Notify Me When checkboxes
+    selectAllNotifyMe: `[data-test-id="selectAllProviderOptions"] [class="RAView"]`,
+    leftCheckbox1: `[data-test-id="visit_submitted_by_patientCheckBox"] [class="RAView"]`,
+    leftCheckbox2: `[data-test-id="scheduled_visit_ready_to_joinCheckBox"] [class="RAView"]`,
+    leftCheckbox3: `[data-test-id="visit_canceled_by_patientCheckBox"] [class="RAView"]`,
+    leftCheckbox4: {selector:`[data-test-id="new_visit_scheduled_by_patientCheckBox"] [class="RAView"]`, index: 0},
+    leftCheckbox5: {selector:`[data-test-id="patient_reschedules_scheduled_visitCheckBox"] [class="RAView"]`, index: 0},
+    leftCheckbox6: {selector: `[data-test-id="scheduled_visit_canceled_by_patientCheckBox"] [class="RAView"]`, index:0 },
+
+    //Notify My Patients checkboxes
+    selectAllNotifyMyPatients: `[data-test-id="selectAllPatientOptions"] [class="RAView"]`,
+    rightCheckbox1: {selector:`[data-test-id="new_visit_scheduled_by_patientCheckBox"] [class="RAView"]`, index: 1},
+    rightCheckbox2: {selector:`[data-test-id="patient_reschedules_scheduled_visitCheckBox"] [class="RAView"]`, index: 1},
+    rightCheckbox3: `[data-test-id="provider_schedules_visit_for_exisiting_patientCheckBox"] [class="RAView"]`,
+    rightCheckbox4: `[data-test-id="provider_reschedules_scheduled_visitCheckBox"] [class="RAView"]`,
+    rightCheckbox5: {selector: `[data-test-id="scheduled_visit_canceled_by_patientCheckBox"] [class="RAView"]`, index:1 },
+    rightCheckbox6: `[data-test-id="scheduled_visit_reminder_a_day_beforeCheckBox"] [class="RAView"]`,
+    rightCheckbox7: `[data-test-id="scheduled_visit_reminder_on_the_day_of_visitCheckBox"] [class="RAView"]`,
+    rightCheckbox8: `[data-test-id="visit_started_by_physicianCheckBox"] [class="RAView"]`,
+    rightCheckbox9: `[data-test-id="visit_requeued_by_physicianCheckBox"] [class="RAView"]`,
+    rightCheckbox10: `[data-test-id="visit_completed_by_physicianCheckBox"] [class="RAView"]`,
 
     saveChangesButton: `[data-test-id="saveNotificationChanges"]`,
     saveChangesSpinner: `[class="applicationActivityIndicator"]`
@@ -49,7 +73,7 @@ const commands = [{
         this.accessNotificationsSection()
         return this
     },
-
+    //Access Notification Section considering the current page is My Account
     accessNotificationsSection(){
         this
             .waitForElementVisible('@notificationsSection')
@@ -58,6 +82,23 @@ const commands = [{
             .waitForElementVisible('@notificationsSectionSelected')
         return this
     },
+
+    //Perform the logout, login and access Notification Page again
+    accessNotificationsSectionAfterLogout(email, password){
+        this
+            const loginPage = this.api.page.loginPage()
+            const waitingRoomPage = this.api.page.provider.waitingRoomPage()
+            // Logout and Login again to check persistence
+            waitingRoomPage.selectLogout()
+            waitingRoomPage.acceptLogout()
+            loginPage.userLogin(email, password)
+
+            //access Notifications Page
+            waitingRoomPage.selectMyAccount()
+            this.accessNotificationsSection()
+        return this
+    },
+
     //checks if the first 3 channels have the values equals to the ones sent by parameter
     checkNotificationPersistence(emailNotifValue, phoneNotifValue, textNotifValue){
         return this
@@ -66,11 +107,13 @@ const commands = [{
             .verify.attributeEquals('@smsNotifField', 'value', textNotifValue)
     },
 
-    //value = 'right' means enabled, value = 'left' means disabled
+    /* Check status of toggle
+    Where value = 'right' means enabled, value = 'left' means disabled */
     checkToggleStatus(toggleLocator, value){
         return this.verify.attributeContains(toggleLocator, 'style', value)
     },
-    
+
+    //Toggle channel and verify if the toggle has the new status after saving
     toggleNotifChannelandCheck(toggleLocator){
         return this.getAttribute(toggleLocator, 'style', (result) => {
             if(result.value.includes('right')){ //it means channel is enabled
@@ -87,7 +130,8 @@ const commands = [{
            }
         })
     },
-    
+
+    //Add Email Notification Channel
     addEmailNotification(email){
         return this
             .click('@addNotificationButton')
@@ -100,6 +144,7 @@ const commands = [{
             .verify.attributeEquals('@customEmailNotifField', 'value', email)
     },
 
+    //Add Phone Notification Channel
     addPhoneNotification(phone){
         return this
             .waitForElementVisible('@addNotificationButton')
@@ -113,6 +158,7 @@ const commands = [{
             .verify.attributeEquals('@customPhoneNotifField', 'value', phone)
     },
 
+    //Add SMS Notification Channel
     addSMSNotification(phone){
         return this
             .waitForElementVisible('@addNotificationButton')
@@ -126,6 +172,7 @@ const commands = [{
             .verify.attributeEquals('@customSMSNotifField', 'value', phone)
     },
 
+    //Remove a given channel (only works with custom channels)
     removeChannel(toggleLocator){
         return this
             .click(toggleLocator)
@@ -134,6 +181,7 @@ const commands = [{
             .click('@removeChannelButton')
     },
 
+    //Remove the added custom channels and verify if they are not present anymore
     removeCustomChannels(){
         return this
             .removeChannel('@customSmsNotifToggle')
@@ -144,7 +192,128 @@ const commands = [{
             .waitForElementNotPresent('@customSmsNotifToggle')
             .waitForElementNotPresent('@customPhoneNotifToggle')
             .waitForElementNotPresent('@customEmailNotifToggle')
-    }
+    },
+
+    //Check if the checkbox has 'selected' color in case isSelect == true OR 'not selected' color if isSelected == false
+    //Using 'assert' to prevent test continuing if the first verified checkbox is not as expected
+    assertCheckboxStatus(checkbox, selectedStatus) {
+        if (selectedStatus == true) {
+            this.assert.cssProperty(checkbox, 'border-color', 'rgb(42, 178, 188)')
+        }
+        else if (selectedStatus == false) {
+            this.assert.cssProperty(checkbox, 'border-color', 'rgb(173, 177, 179)')
+        }
+        return this
+    },
+
+    //Click on Select All checkbox and isSelected gets the new status of checkbox
+    selectAll(checkbox, isSelected){
+        this
+            .waitForElementVisible(checkbox)
+            //gets the status of Select All checkbox (selected/unselected)
+            .getAttribute(checkbox, 'style', (result) => {
+                if(result.value.includes('border-color: rgb(42, 178, 188)')){ //it means checkbox is selected
+                    this.click(checkbox)
+                        .click('@saveChangesButton')
+                        .waitForElementNotVisible('@saveChangesSpinner')
+                    isSelected (false); // receives false because it will change to UNSELECTED after click
+                }
+                else { //it means checkbox is not selected
+                    this.click(checkbox)
+                        .click('@saveChangesButton')
+                        .waitForElementNotVisible('@saveChangesSpinner')
+                    isSelected (true); // receives true because it will change to SELECTED after click
+                }
+            }) 
+        return this
+    },
+
+    //Check if checkboxes changed its color according to isSelected status
+    checkPersistenceOfSelectAllNotifyMe(isSelected){
+        console.log("Checking changes in un/selected checkboxes")
+        this.assertCheckboxStatus('@selectAllNotifyMe', isSelected)
+        this.assertCheckboxStatus('@leftCheckbox1', isSelected)
+        this.assertCheckboxStatus('@leftCheckbox2', isSelected)
+        this.assertCheckboxStatus('@leftCheckbox3', isSelected)
+        this.assertCheckboxStatus('@leftCheckbox4', isSelected)
+        this.assertCheckboxStatus('@leftCheckbox5', isSelected)
+        this.assertCheckboxStatus('@leftCheckbox6', isSelected)
+        return this
+    },
+
+    //Check if checkboxes changed its color according to isSelected status
+    checkPersistenceOfSelectAllNotifyMyPatients(isSelected){
+        console.log("Checking changes in un/selected checkboxes")
+        this.assertCheckboxStatus('@selectAllNotifyMyPatients', isSelected)
+        this.assertCheckboxStatus('@rightCheckbox1', isSelected)
+        this.assertCheckboxStatus('@rightCheckbox2', isSelected)
+        this.assertCheckboxStatus('@rightCheckbox3', isSelected)
+        this.assertCheckboxStatus('@rightCheckbox4', isSelected)
+        this.assertCheckboxStatus('@rightCheckbox5', isSelected)
+        this.assertCheckboxStatus('@rightCheckbox6', isSelected)
+        this.assertCheckboxStatus('@rightCheckbox7', isSelected)
+        this.assertCheckboxStatus('@rightCheckbox8', isSelected)
+        this.assertCheckboxStatus('@rightCheckbox9', isSelected)
+        this.assertCheckboxStatus('@rightCheckbox10', isSelected)
+        return this
+    },
+
+    //Select a checkbox and disable the correspondent Select All checkbox to let only the given checkbox selected
+    selectOnlyOne(checkbox,checkboxAll){
+        this
+            .waitForElementVisible(checkboxAll)
+            //gets the status of Select All checkbox (selected/unselected)
+            .getAttribute(checkboxAll, 'style', (result) => {
+                if(result.value.includes('border-color: rgb(42, 178, 188)')){ //it means the Select All checkbox  is selected
+                    this.click(checkboxAll) //Unselect All
+                        .click(checkbox) //Select only the target checkbox
+                        .click('@saveChangesButton')
+                        .waitForElementNotVisible('@saveChangesSpinner')
+                }
+                else { //it means the Select All checkbox is not selected
+                    this.click(checkboxAll) //Select All
+                        .click(checkboxAll) //Unselect All again to make sure no other checkbox was selected
+                        .click(checkbox) //Select only the target checkbox
+                        .click('@saveChangesButton')
+                        .waitForElementNotVisible('@saveChangesSpinner')
+                }
+            }) 
+        return this
+    },
+    /* 
+    This method checks if only the previously selected checkbox is enabled
+    To this test we consider that only the first checkbox is selected
+    */
+    checkPersistenceOfSelectOnlyOneNotifyMe(){
+        console.log("Checking changes after selecting only one checkbox")
+        this.assertCheckboxStatus('@selectAllNotifyMe', false)
+        this.assertCheckboxStatus('@leftCheckbox1', true)
+        this.assertCheckboxStatus('@leftCheckbox2', false)
+        this.assertCheckboxStatus('@leftCheckbox3', false)
+        this.assertCheckboxStatus('@leftCheckbox4', false)
+        this.assertCheckboxStatus('@leftCheckbox5', false)
+        this.assertCheckboxStatus('@leftCheckbox6', false)
+        return this
+    },
+    /* 
+    This method checks if only the previously selected checkbox is enabled
+    To this test we consider that only the first checkbox is selected
+    */
+    checkPersistenceOfSelectOnlyOneNotifyMyPatients(){
+        console.log("Checking changes after selecting only one checkbox")
+        this.assertCheckboxStatus('@selectAllNotifyMyPatients', false)
+        this.assertCheckboxStatus('@rightCheckbox1', true)
+        this.assertCheckboxStatus('@rightCheckbox2', false)
+        this.assertCheckboxStatus('@rightCheckbox3', false)
+        this.assertCheckboxStatus('@rightCheckbox4', false)
+        this.assertCheckboxStatus('@rightCheckbox5', false)
+        this.assertCheckboxStatus('@rightCheckbox6', false)
+        this.assertCheckboxStatus('@rightCheckbox7', false)
+        this.assertCheckboxStatus('@rightCheckbox8', false)
+        this.assertCheckboxStatus('@rightCheckbox9', false)
+        this.assertCheckboxStatus('@rightCheckbox10', false)
+        return this
+    },
 
 }];
 
--- tests/provider/my_account/notifications_test.js
+++ tests/provider/my_account/notifications_test.js
@@ -1,22 +1,18 @@
-let settingsPage;
-
 module.exports = {
     
     before: function (browser) {
-        browser.resizeWindow(1920, 1080);
+        browser.resizeWindow(1920, 1300);
         '@tags:'['test']
-        notificationsPage = browser.page.provider.my_account.notificationsPage();
+        notificationsPage = browser.page.provider.my_account.notificationsPage()
+        notificationsPage.accessNotificationsPage(browser.globals.providerEmail, browser.globals.providerPassword)
     },
     
     "Edit Notification Channels": function (browser) {
-        
-        notificationsPage.accessNotificationsPage(browser.globals.providerEmail, browser.globals.providerPassword)
-            //edit notification fields
-            .editTextField('@emailNotifField', "test@evisit.com")
-            .editTextField('@phoneNotifField', "555-555-5555")
-            .editTextField('@smsNotifField', "555-555-5555")
-            .click('@saveChangesButton')
-        
+        //edit notification fields
+        notificationsPage.editTextField('@emailNotifField', "test@evisit.com")
+        notificationsPage.editTextField('@phoneNotifField', "555-555-5555")
+        notificationsPage.editTextField('@smsNotifField', "555-555-5555")
+        notificationsPage.click('@saveChangesButton')
         //TODO: check confirmation toast when it exists - Bug #8aka6k
 
         //assert changes persistence
@@ -24,24 +20,57 @@ module.exports = {
     },
 
     "Toogle Notification Channels": function (browser) {
-        //notificationsPage.accessNotificationsPage(browser.globals.providerEmail, browser.globals.providerPassword)
         notificationsPage.toggleNotifChannelandCheck('@smsNotifToggle')
         notificationsPage.toggleNotifChannelandCheck('@phoneNotifToggle')
         notificationsPage.toggleNotifChannelandCheck('@emailNotifToggle')
-        
         //TODO: check confirmation toast when it exists - Bug #8aka6k
+        //TODO: check persistence - Depends on #275v9m - Verified with CSS workaround
     },
 
     "Add Notifications Channels": function (browser){
-        //notificationsPage.accessNotificationsPage(browser.globals.providerEmail, browser.globals.providerPassword)
         notificationsPage.addEmailNotification("newchannel@evisit.com")
         notificationsPage.addPhoneNotification("111-111-1111")
         notificationsPage.addSMSNotification("222-222-2222")
+        //TODO: check confirmation toast when it exists - Bug #8aka6k
     },
 
     "Remove Notification Channels": function (browser) {
-        //notificationsPage.accessNotificationsPage(browser.globals.providerEmail, browser.globals.providerPassword)
         notificationsPage.removeCustomChannels()
+        //TODO: check confirmation toast when it exists - Bug #8aka6k
+    },
+
+    "Test Select All Notify Me When Checkboxes": function (browser) {
+        notificationsPage.selectAll('@selectAllNotifyMe',  isSelected => {
+            //Logout, login and access Notifications Page again to check persistence
+            notificationsPage.accessNotificationsSectionAfterLogout(browser.globals.providerEmail, browser.globals.providerPassword)
+            //Check if the changes were saved after logout
+            notificationsPage.checkPersistenceOfSelectAllNotifyMe(isSelected)
+        })
+    },
+
+    "Test Select All Notify My Patients Checkboxes": function (browser) {
+        notificationsPage.selectAll('@selectAllNotifyMyPatients',  isSelected => {
+            //Logout, login and access Notifications Page again to check persistence
+            notificationsPage.accessNotificationsSectionAfterLogout(browser.globals.providerEmail, browser.globals.providerPassword)
+            //Check if the changes were saved after logout
+            notificationsPage.checkPersistenceOfSelectAllNotifyMyPatients(isSelected)
+        })
+    },
+
+    "Test Select Only One of Notify Me When Checkboxes": function (browser) {
+        notificationsPage.selectOnlyOne('@leftCheckbox1', '@selectAllNotifyMe')
+        //Logout, login and access Notifications Page again to check persistence
+        notificationsPage.accessNotificationsSectionAfterLogout(browser.globals.providerEmail, browser.globals.providerPassword)
+        //Check if the changes were saved after logout
+        notificationsPage.checkPersistenceOfSelectOnlyOneNotifyMe()
+    },
+
+    "Test Select Only One of Notify My Patients Checkboxes": function (browser) {
+        notificationsPage.selectOnlyOne('@rightCheckbox1', '@selectAllNotifyMyPatients')
+        //Logout, login and access Notifications Page again to check persistence
+        notificationsPage.accessNotificationsSectionAfterLogout(browser.globals.providerEmail, browser.globals.providerPassword)
+        //Check if the changes were saved after logout
+        notificationsPage.checkPersistenceOfSelectOnlyOneNotifyMyPatients()
     },
 
     after: function (browser) {
